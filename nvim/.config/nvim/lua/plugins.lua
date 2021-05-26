e59af77242f0c9ce4045944a6bfaabf8a75e9306
David Richardson <drichardson42@gatech.edu>
2021-05-26 18:03:55 -0400

    Add git support to neovim

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,8 +1,7 @@
 -- Auto install packer.nvim if not exists
 local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  vim.api.nvim_command("!git clone https://github.com/wbthomason/packer.nvim " ..
-                         install_path)
+  vim.api.nvim_command("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
 end
 
 vim.cmd("packadd packer.nvim")
@@ -19,4 +18,7 @@ return require("packer").startup(function()
   -- Parenentheses, brackets, etc.
   use "windwp/nvim-autopairs" -- automatic (), [], "", etc.
   use "p00f/nvim-ts-rainbow" -- rainbow parentheses
+
+  use "tpope/vim-fugitive" -- git command integration
+  use {"lewis6991/gitsigns.nvim", requires = {"nvim-lua/plenary.nvim"}} -- show git changes in sign col
 end)
