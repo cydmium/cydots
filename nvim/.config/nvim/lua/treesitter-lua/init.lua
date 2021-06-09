0c32beb5aab71e9255f883791e08fc56ed3b9c11
David Richardson <davidrichardson96@gmail.com>
2021-06-09 16:17:15 -0400

    Temporarily disable TS indentation for python

--- nvim/.config/nvim/lua/treesitter-lua/init.lua
+++ nvim/.config/nvim/lua/treesitter-lua/init.lua
@@ -1,6 +1,6 @@
 require"nvim-treesitter.configs".setup {
   ensure_installed = {"python", "cpp", "c", "lua", "latex", "bash", "yaml", "bibtex"},
   highlight = {enable = true, disable = {"latex"}},
-  indent = {enable = true},
+  indent = {enable = true, disable = {"python"}},
   autopairs = {enable = true}
 }
