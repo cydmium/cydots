70075ae5f3fd81f510ed960cc678c734a3b27253
David Richardson <davidrichardson96@gmail.com>
2024-07-26 15:54:12 -0600

    feat: Add space around upstream change prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -16,8 +16,8 @@ function fish_right_prompt --description 'Write out the right prompt'
 
     set -g __fish_git_prompt_color_branch EE8B4E
     set -g __fish_git_prompt_showupstream "informative"
-    set -g __fish_git_prompt_char_upstream_ahead "↑"
-    set -g __fish_git_prompt_char_upstream_behind "↓"
+    set -g __fish_git_prompt_char_upstream_ahead " ↑"
+    set -g __fish_git_prompt_char_upstream_behind " ↓"
     set -g __fish_git_prompt_char_upstream_prefix ""
 
     set -g __fish_git_prompt_char_stagedstate " ●"
